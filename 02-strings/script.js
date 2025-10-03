caf109b7876d9ff1f53fbf527af864f60bf8ae0c
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-10-03 13:15:43 +0200

    strings-2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -10,6 +10,15 @@ console.log(myName);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+const number = 1
+console.log(number);
+
+const text = "ciao"
+console.log(text)
+
+const bool = true
+console.log(bool)
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
